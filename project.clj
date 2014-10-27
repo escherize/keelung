f71e8fbe3e45f6166b1fd7f0391e00492712ce16
Bryan Maass <bryan.maass@gmail.com>
2014-10-26 21:40:19 -0700

    added some code, added the /search route
    - which takes comma seperated query params

--- project.clj
+++ project.clj
@@ -4,9 +4,14 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler keelung.core.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                 [ring/ring-defaults "0.1.2"]
+                 [crajure "0.1.0-SNAPSHOT"]
+                 [cheshire "5.3.1"]]
+  :plugins [[lein-ring "0.8.13"]
+            [cider/cider-nrepl "0.7.0"]]
+  :ring {:handler keelung.core.handler/app
+         :nrepl {:start? true
+                 :port 7171}}
+  :profiles {:dev
+             {:dependencies [[javax.servlet/servlet-api "2.5"]
+                             [ring-mock "0.1.5"]]}})
--- src/keelung/core/handler.clj
+++ src/keelung/core/handler.clj
@@ -1,10 +1,22 @@
 (ns keelung.core.handler
-  (:require [compojure.core :refer :all]
+  (:require [clojure.string :as str]
+            [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [crajure.core :as cl]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]
+            [cider.nrepl :as cider]))
+
+(defn search-sf-for-sale [req]
+  (let [items  (-> (get (:query-params req) "terms" "")
+                   (str/split #","))]
+    (cl/query-cl {:query (str/join " " items)
+                  :section :for-sale
+                  :area "sfbay"})))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/search" [] #(search-sf-for-sale %))
   (route/not-found "Not Found"))
 
 (def app
